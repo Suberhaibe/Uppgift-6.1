bfa84708bd67d6a83ba3fd7c0b3ebaa37fec3f58
Admin <Admin@TEB127008>
2023-02-10 10:42:52 +0100

    Add project files.

---
+++ Uppgift 6.1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Uppgift_6._1
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+
+            int tal1 = 2;
+
+            int tal2 = 1;
+            int tal3 = 23;
+            Addera(tal1, tal2, tal3);
+
+        }
+
+        static void Addera(int tal1, int tal2, int tal3)
+        {
+
+            Console.WriteLine(tal1 + tal2 + tal3);
+        }
+    }
+}
